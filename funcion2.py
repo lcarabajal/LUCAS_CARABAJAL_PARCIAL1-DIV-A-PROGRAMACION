beca7a704ed7ea5bb937ef2f23a04af677acb40f
lcarabajal <lucascarabajal46@gmail.com>
2023-05-22 10:44:58 -0300

    Parcial Terminado

--- funcion2.py
+++ funcion2.py
@@ -1,1 +1,25 @@
-print("Funcion 2")
+# 2. Crear una función que se llame reemplazarCaracteres que reciba una cadena de caracteres como primer parámetro, 
+# un carácter como segundo y otro carácter  como tercero,  
+# la misma deberá reemplazar cada ocurrencia del segundo parámetro por el tercero y devolver la cantidad de veces que se reemplazo ese carácter 
+# en la cadena
+
+import re
+
+
+def reemplazarCaracteres(cadena:str,caracter1:str,caracter2:str)->str:
+    contador = 0 
+    lista_caracteres = []
+    for caracter in cadena:
+        if re.match(caracter1,caracter):
+            contador += 1
+            caracter = caracter2
+            lista_caracteres.append(caracter)
+        else:
+            caracter = ""
+            lista_caracteres.append(caracter)    
+            
+    lista_caracteres.join()    
+    print(lista_caracteres)
+    print(contador)
+    
+reemplazarCaracteres("Esto es una prueba","e","l")
--- funcion3.py
+++ funcion3.py
@@ -1,1 +1,18 @@
-print("funcion 3")
+# 3. Crear una función que se llame ordenarCaracteres que reciba 
+# una cadena de caracteres como parámetro  y su responsabilidad es ordenar 
+# los caracteres de manera ascendente dentro de la cadena. Ejemplo si le pasamos "algoritmo" la deja como "agilmoort"
+
+def ordenarCaracteres(cadena:str) -> str:
+    tam = len(cadena)
+            
+    for i in range(tam - 1):
+        for j in range (i + 1, tam):
+            if (cadena[i] > cadena[j]):  
+                aux = cadena[i]
+                cadena[i] = cadena[j]     
+                cadena[j] = aux   
+                
+    print(cadena)
+    
+
+ordenarCaracteres("algoritmo")
